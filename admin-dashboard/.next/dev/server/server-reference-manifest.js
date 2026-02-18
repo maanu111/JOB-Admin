d0af407728128e173fc2a2865068ea7367693aea
Himanshu Thakur <himanshumaanu111@gmail.com>
2026-02-18 22:00:06 +0530

    added admin dashboard properly

---
+++ admin-dashboard/.next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Y0UgErJcGYyzZBgBi8ozgL/m0oOONSRB1jXrjqZleKE=\"\n}"
---
+++ admin-dashboard/.next/dev/static/chunks/Desktop_Starter-Admin_admin-dashboard_app_page_tsx_eb778d0f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/9d0ff_next_2634e6bf._.js",
+  "static/chunks/Desktop_Starter-Admin_admin-dashboard_app_page_tsx_25db857a._.js"
+],
+    source: "dynamic"
+});
