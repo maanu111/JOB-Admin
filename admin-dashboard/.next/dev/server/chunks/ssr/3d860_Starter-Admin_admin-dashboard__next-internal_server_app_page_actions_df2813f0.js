0ec6ceed4f0ccf89ff420d44ed688320e3d9a3af
Himanshu Thakur <himanshumaanu111@gmail.com>
2026-02-18 22:03:41 +0530

    removed next build files

--- admin-dashboard/.next/dev/server/chunks/ssr/3d860_Starter-Admin_admin-dashboard__next-internal_server_app_page_actions_df2813f0.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/Desktop/Starter-Admin/admin-dashboard/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=3d860_Starter-Admin_admin-dashboard__next-internal_server_app_page_actions_df2813f0.js.map
